6be8b818f30022999d35ea2cea273f5c4204e37d
zhaozhe <zhaozhe05@meituan.com>
2018-12-03 23:05:52 +0800

    做一些改动，以理解插入排序是原地排序算法

--- algorithms/Sort/InsertionSort.js
+++ algorithms/Sort/InsertionSort.js
@@ -1,25 +1,23 @@
 
 const InsertionSort = (unsortedArr) => {
     const length = unsortedArr.length;
-    const sortedArr = unsortedArr.concat();
-    if (length < 1) {
+    if (length <= 1) {
         return sortedArr;
     }
     for (let i = 1; i < length; ++i) {
-        const value = sortedArr[i];
+        const value = unsortedArr[i];
         let j = i - 1;
         for (; j >=  0; --j) {
-            if (sortedArr[j] > value) {
-                sortedArr[j+1] = sortedArr[j];
+            if (unsortedArr[j] > value) {
+                unsortedArr[j+1] = unsortedArr[j];
             } else {
                 break;
             }
         }
-        sortedArr[j+1] = value;
+        unsortedArr[j+1] = value;
     }
-    return sortedArr;
 }
 
 let arr = [4, 5, 6, 3, 2, 1];
-
-console.log(InsertionSort(arr));
+InsertionSort(arr)
+console.log(arr);
