e59ccb751effeba3e66a8be983310284fe192abd
zhaozhe <zhaozhe05@meituan.com>
2018-12-04 22:23:36 +0800

    选择排序

--- algorithms/Sort/SelectionSort.js
+++ algorithms/Sort/SelectionSort.js
@@ -1,72 +1,27 @@
 /**
  * Created by mason[zhaozhe05@meituan.com] on 2017/12/10.
  */
-const Benchmark = require('benchmark');
-const suite = new Benchmark.Suite;
 
-const SelectionSort1 = (unsortedArray) => {
-    const length = unsortedArray.length;
-    const arr = unsortedArray.slice();
-    let minPosition;
-    let minValue;
-    let swapValue;
-    for (let i = 0; i < length; i++) {
-        minPosition = i;
-        minValue = arr[i];
-        for (let j = i; j < length; j++) {
-            if (arr[j] < minValue) {
-                minValue = arr[j];
-                minPosition = j;
-            }
-        }
-        if (minPosition !== i) {
-            swapValue = arr[i];
-            arr[i] = arr[minPosition];
-            arr[minPosition] = swapValue;
-        }
-    }
-    return arr;
-};
-
-const SelectionSort2 = (unsortedArray) => {
-    const length = unsortedArray.length;
-    const arr = unsortedArray.slice();
-    let minPosition;
-    let minValue;
-    let swapValue;
-    for (let i = 0; i < length; i++) {
-        minPosition = i;
-        minValue = arr[i];
-        for (let j = i; j < length; j++) {
-            if (arr[j] < minValue) {
-                minValue = arr[j];
-                minPosition = j;
-            }
-        }
-        swapValue = arr[i];
-        arr[i] = arr[minPosition];
-        arr[minPosition] = swapValue;
-    }
-    return arr;
-};
-
-const num = 100;
-let unsortedArray = [];
-for (let i = 0; i < num; i++) {
-    unsortedArray[i] = Math.floor(Math.random() * 100);
+const SelectionSort = (unsortedArr) => {
+   const length = unsortedArr.length;
+   let minPosition;
+   let minValue;
+   // 注意这里的边界情况，只需要到 length - 1 即可
+   for (let i = 0; i < length - 1; ++i) {
+       minPosition = i;
+       minValue = unsortedArr[i];
+       let j = i + 1;
+       console.log(j);
+       for ( ;j < length; ++j) {
+           if (minValue > unsortedArr[j]) {
+               minPosition = j;
+               minValue = unsortedArr[j];
+           }
+       }
+       [unsortedArr[i], unsortedArr[minPosition]] = [unsortedArr[minPosition], unsortedArr[i]];
+   }
 }
 
-// console.log('unsortedArray:', unsortedArray);
-// const sortedArray = SelectionSort(unsortedArray);
-// console.log('sortedArray:', sortedArray);
-
-
-// 添加测试
-suite.add('test1', function () {
-    SelectionSort1(unsortedArray);
-}).add('test2', function () {
-    SelectionSort2(unsortedArray);
-}).on('cycle', function (event) {
-    // add listeners
-    console.log(String(event.target));
-}).run({'async': true});
+const arr = [4, 8, 6, 3, 2, 1, 0, 12]
+SelectionSort(arr);
+console.log(arr)
